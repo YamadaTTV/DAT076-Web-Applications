e902325a59ea999024468282d5f8091bb84332b4
rstandar <rassta@student.chalmers.se>
2023-01-31 12:00:48 +0100

    Created router for Product

---
+++ server/src/router/Product.ts
@@ -0,0 +1,42 @@
+import express, {query, Request, Response} from "express";
+import {makeProductService} from "../service/Product";
+import {Product} from "../model/Product";
+import {User} from "../model/User";
+
+const taskService = makeProductService();
+
+export const ProductRouter = express.Router();
+
+ProductRouter.get("/", async (
+    req: Request<{}, {}, {}>,
+    res: Response<Array<Product> | String>
+) => {
+    try {
+        const tasks = await taskService.getProducts();
+        res.status(200).send(tasks);
+    } catch (e: any) {
+        res.status(500).send(e.message);
+    }
+});
+
+ProductRouter.post("/", async (
+    req: Request<{}, {},{productId: number, productName: string, productCategory: string, price: number, seller : User}>,
+    res: Response<Product | string>
+) => {
+    try {
+        let productId = req.body.productId;
+        let productName = req.body.productName;
+        let productCategory = req.body.productCategory;
+        let price = req.body.price;
+        let seller = req.body.seller;
+
+        if (typeof(productId) !== "number" || typeof(productName) !== "string" || typeof(productCategory) !== "string" || typeof(price) !== "number" || typeof(seller) !== "object") {
+            res.status(400).send(`Bad PUT call to ${req.originalUrl}`);
+            return;
+        }
+        const newProduct = await taskService.addProduct(productId,productName,productCategory,price,seller);
+        res.status(201).send(newProduct);
+    } catch (e: any) {
+        res.status(500).send(e.message);
+    }
+});
--- server/src/router/User.ts
+++ server/src/router/User.ts
@@ -23,12 +23,15 @@ userRouter.post("/", async (
     res: Response<User | string>
 ) => {
     try {
-        const id = req.body.id;
-        if (typeof(id) !== "string") {
-            res.status(400).send(`Bad PUT call to ${req.originalUrl} --- description has type ${typeof(id)}`);
+        let id = req.body.id;
+        let username = req.body.username;
+        let email = req.body.email;
+        let password = req.body.password;
+        if (typeof(id) !== "number" || typeof(username) !== "string" || typeof(email) !== "string" || typeof(password) !== "string" ) {
+            res.status(400).send(`Bad PUT call to ${req.originalUrl} --- id has type ${typeof(id)}, username has type ${typeof(username)}, email has type ${typeof(email)} and password has type ${typeof(password)}`);
             return;
         }
-        const newUser = await taskService.addUser(req.body.id,req.body.username,req.body.email,req.body.password);
+        const newUser = await taskService.addUser(id,username,email,password);
         res.status(201).send(newUser);
     } catch (e: any) {
         res.status(500).send(e.message);
