722049c3b8760e182da4734f363443363c814307
YamadaTTV <92035299+YamadaTTV@users.noreply.github.com>
2023-02-01 14:13:02 +0100

    Product unit test update, passes
    Product unit test update, passes

--- server/src/service/Product.test.ts
+++ server/src/service/Product.test.ts
@@ -12,3 +12,21 @@ test("If a product is added to the list then it should be in the list", async() 
     const products = await productService.getProducts();
     expect(products.some((product) => product.productName === productName)).toBeTruthy();
 });
+
+test("If a product is updated, then its values should be updated", async() => {
+    const productService = makeProductService();
+    const productName = "Product 1";
+    const productCategory = "Category 1";
+    const price = 100;
+    const sellerId = 1;
+    const addProduct = await productService.addProduct(productName, productCategory, price, sellerId);
+    const updateProductName = "Updated Product 1";
+    const updateResult = await productService.updateProduct(addProduct.productId, updateProductName);
+    expect(updateResult).toBeTruthy();
+    
+    const products = await productService.getProducts();
+    const updateProduct = products.find((product) => product.productId === addProduct.productId);
+    if(updateProduct){
+        expect(updateProduct.productName).toEqual(updateProductName);
+    }
+});
