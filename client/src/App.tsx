c4c465d3595a3c860770628ca7c1d5d0e44bf2bb
Pipipipiano <marcusphu@gmail.com>
2023-02-14 11:26:40 +0100

    Added a register form
    Added so the button register pops a register form up.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {Card, Button,Row,Col} from 'react-bootstrap'
 import {render} from "react-dom";
 
 function App() {
-  const soffa: IProduct = {productId:1,productName:"soffa",productCategory:"möbel",price:123,seller:1}
-  return (
-      <div className="Row">
-          <div className="Col-6">
-              {Product(soffa)}
-          </div>
-          <div className="Col-6">
-              {Product(soffa)}
-          </div>
-      </div>
-  );
+    const soffa: IProduct = {productId:1,productName:"soffa",productCategory:"möbel",price:123,seller:1}
+    const[formOpen, setformOpen] = useState(false);
+    const handleClick = () => {
+        setformOpen(!formOpen);
+    }
+    return (
+
+        RegisterUser(handleClick, formOpen)
+    );
 }
 interface IProduct {
   productId: number,
@@ -30,12 +28,12 @@ interface IProduct {
 function Product ({productId,productName,productCategory,price,seller} : IProduct){
   return (
       <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Img variant="top" src="client/src/murrayPog.png" />
         <Card.Body>
-          <Card.Title>Card Title</Card.Title>
+          <Card.Title>{productName}</Card.Title>
           <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
+            [Some quick example text to build on the card title and make up the
+            bulk of the card's content.]
           </Card.Text>
           <Button variant="primary">Go somewhere</Button>
         </Card.Body>
@@ -43,10 +41,59 @@ function Product ({productId,productName,productCategory,price,seller} : IProduc
   );
 }
 
-function RegisterUser (){
+function RegisterUser (handleClick : () => void, formOpen : boolean){
   return (
-      <p>Hello</p>
+      <div>
+          <h1>Welcome to Marketplace!</h1>
+              <form>
+                  <div>
+                      <input type="text" id="username" name="username"
+                             placeholder="Username">
+                      </input>
+                  </div>
+                  <div>
+                      <input type="password" id="password" name="password"
+                             placeholder="Password">
+                      </input>
+                  </div>
+                  <div>
+                      <input type="submit" value="Login" id="submitBtn"></input>
+                  </div>
+                  <div>
+                      <hr></hr>
+                      <span>OR</span>
+                      <hr></hr>
+                  </div>
+                  <div>
+                      <button type="button" onClick={handleClick} >Register</button>
+                      {formOpen && <div>{registerForm()}</div>}
+                  </div>
+              </form>
+      </div>
   );
+}
+
+function registerForm(){
+    return(
+        <div>
+            <h1>Register!</h1>
+            <form>
+                <div>
+                    <input type="text" id="username" name="username"
+                           placeholder="Username">
+                    </input>
+                </div>
+                <div>
+                    <input type="password" id="password" name="password"
+                           placeholder="Password">
+                    </input>
+                </div>
+                <div>
+                    <input type="submit" value="Register" id="submitBtn"></input>
+                </div>
+            </form>
+        </div>
+    );
 }
 
 export default App;
