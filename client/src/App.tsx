de9431b59ccc7b1f91c947046680a4592fe03817
Pipipipiano <marcusphu@gmail.com>
2023-02-18 20:58:19 +0100

    Fixed index.html
    Fixed the whole frontend for index.html

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -51,30 +51,26 @@ function App() {
 
     if(loggedIn){
         return (
-
-
             //Change to return the marketplace.
             <div>
                 <Header loggedIn={loggedIn} handleLoginClick={handleLoginClick} handleSellClick={handleSellClick}/>
-
                 <ProductPage products={products}/>
+                <Footer/>
             </div>
         );
     }
-
-    if(!formOpen){
+    else{
         return(
             <div>
-                <Header loggedIn={loggedIn} handleLoginClick = {handleLoginClick} handleSellClick={handleSellClick}/>
-                <LoginForm handleLoginClick={handleLoginClick} handleRegisterClick={handleRegisterClick} />
-            </div>);
-    } else {
-        return(
-        <div>
-            <Header loggedIn={loggedIn} handleLoginClick = {handleLoginClick} handleSellClick={handleSellClick}/>
-            <RegisterForm handleRegisterClick={handleRegisterClick}/>
-        </div>);
+                <Header loggedIn={loggedIn} handleLoginClick={handleLoginClick} handleSellClick={handleSellClick}/>
+                <IndexPage handleLoginClick = {handleLoginClick} handleRegisterClick={handleRegisterClick} handleSellClick={handleSellClick} loggedIn={loggedIn} formOpen={formOpen}></IndexPage>
+                <Footer></Footer>
+            </div>
+        )
     }
+
+
+
 }
 
 /**
@@ -136,10 +132,10 @@ function Product (props:  {children : object, prod : IProduct}){
               </Card.Text>
               <Row>
                   <Col xs={4}>
-                      <Button variant="primary">Buy</Button>
+                      <Button variant="btn-primary">Buy</Button>
                   </Col>
                   <Col xs={8}>
-                      <Button variant="primary">Contact seller</Button>
+                      <Button variant="btn-primary">Contact seller</Button>
                   </Col>
               </Row>
           </Card.Body>
@@ -197,6 +193,16 @@ function Header(props:{loggedIn:Boolean, handleLoginClick : () => void, handleSe
     }
 }
 
+function Footer(){
+    return(
+        <nav className="navbar navbar-expand-xl footer fixed-bottom">
+            <div className="text-center footerText">
+                All Rights Reserved © 2023 Liam, Rasmus & Marcus
+            </div>
+        </nav>
+        )
+}
+
 /**
  * The component for the LoginForm, shows a login menu
  * @param handleLoginClick - handler for when login button is pressed.
@@ -208,43 +214,45 @@ function LoginForm(props: {handleLoginClick : () => void, handleRegisterClick : 
     const[password, setpassword] = useState("");
         return (
             <div>
-                <h1>Welcome to Marketplace!</h1>
-                <form onSubmit={
-                    async e => {
-                        e.preventDefault();
-                        let response = await axios.post("http://localhost:8080/user/login",{ username:username,password:password})
-                        if(response.status == 400){
-                            //Mark something as wrong.
-                        } else if (response.status == 202){
-                            console.log("Success")
-                            props.handleLoginClick()
+                <div className={"login-container"}>
+                    <h1 className={"login-form"}>Welcome to Marketplace!</h1>
+                    <form className={"login-form"} onSubmit={
+                        async e => {
+                            e.preventDefault();
+                            let response = await axios.post("http://localhost:8080/user/login",{ username:username,password:password})
+                            if(response.status == 400){
+                                //Mark something as wrong.
+                            } else if (response.status == 202){
+                                console.log("Success")
+                                props.handleLoginClick()
+                            }
                         }
-                    }
-                }>
-                    <div>
-                        <input type="text" id="username" name="username"
-                               placeholder="Username" onChange={e => {
-                            setusername(e.target.value);
-                        }}></input>
-                    </div>
-                    <div>
-                        <input type="password" id="password" name="password"
-                               placeholder="Password" onChange={e => {
-                            setpassword(e.target.value);
-                        }}></input>
-                    </div>
-                    <div>
-                        <input type="submit" value="Login" id="submitBtn"></input>
-                    </div>
-                    <div>
-                        <hr></hr>
-                        <span>OR</span>
-                        <hr></hr>
-                    </div>
-                    <div>
-                        <button type="button" onClick={props.handleRegisterClick}>Register</button>
-                    </div>
-                </form>
+                    }>
+                        <div className={"login-div"}>
+                            <input className={"login-form-input"} type="text" id="username" name="username"
+                                   placeholder="Username" onChange={e => {
+                                setusername(e.target.value);
+                            }}></input>
+                        </div>
+                        <div className={"login-div"}>
+                            <input className={"login-form-input"} type="password" id="password" name="password"
+                                   placeholder="Password" onChange={e => {
+                                setpassword(e.target.value);
+                            }}></input>
+                        </div>
+                        <div>
+                            <input className={"btn-primary"} type="submit" value="Login" id="submitBtn"></input>
+                        </div>
+                        <div className={"text-center"}>
+                            <hr></hr>
+                            <span>OR</span>
+                            <hr></hr>
+                        </div>
+                        <div>
+                            <button className={"btn-primary"} type="button" onClick={props.handleRegisterClick}>Register</button>
+                        </div>
+                    </form>
+                </div>
             </div>
         );
 }
@@ -262,41 +270,43 @@ function RegisterForm(props: {handleRegisterClick : () => void}){
     //const[repeatPassword, setrepeatPassword] = useState("");
     return(
         <div>
-            <h1>Register!</h1>
+            <div className={"login-container"}>
+                <h1 className={"login-form"}>Register!</h1>
 
-            <form
-                onSubmit={
-                    async e => {
-                        e.preventDefault();
-                        await axios.post("http://localhost:8080/user",{ username:username,password:password, email:email})
-                        props.handleRegisterClick()
-                    }
-                }>
-                <div>
-                    <input type="text" id="username" name="username"
-                           placeholder="Username" onChange={e => {
-                        setusername(e.target.value);
-                    }}>
-                    </input>
-                </div>
-                <div>
-                    <input type="text" id="email" name="email"
-                           placeholder="Email" onChange={e => {
-                        setemail(e.target.value);
-                    }}>
-                    </input>
-                </div>
-                <div>
-                    <input type="password" id="password" name="password"
-                           placeholder="Password" onChange={e => {
-                        setpassword(e.target.value);
-                    }}>
-                    </input>
-                </div>
-                <div>
-                    <input type="submit" value="Register" id="submitBtn"></input>
-                </div>
-            </form>
+                <form className={"login-form"}
+                    onSubmit={
+                        async e => {
+                            e.preventDefault();
+                            await axios.post("http://localhost:8080/user",{ username:username,password:password, email:email})
+                            props.handleRegisterClick()
+                        }
+                    }>
+                    <div className={"login-div"}>
+                        <input className={"login-form-input"} type="text" id="username" name="username"
+                               placeholder="Username" onChange={e => {
+                            setusername(e.target.value);
+                        }}>
+                        </input>
+                    </div>
+                    <div className={"login-div"}>
+                        <input className={"login-form-input"} type="text" id="email" name="email"
+                               placeholder="Email" onChange={e => {
+                            setemail(e.target.value);
+                        }}>
+                        </input>
+                    </div>
+                    <div className={"login-div"}>
+                        <input className={"login-form-input"} type="password" id="password" name="password"
+                               placeholder="Password" onChange={e => {
+                            setpassword(e.target.value);
+                        }}>
+                        </input>
+                    </div>
+                    <div>
+                        <input className={"btn-primary"} type="submit" value="Register" id="submitBtn"></input>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 }
@@ -358,5 +368,50 @@ function SellForm(props: {handleSellClick : () => void}){
         </div>
     );
 }
+
+function IndexPage(props: {handleLoginClick : () => void, handleRegisterClick : () => void, handleSellClick : () => void, loggedIn : Boolean, formOpen : Boolean}){
+    const wallpaper = require("./wallpaperflare.com_wallpaper.jpg");
+    return(
+        <div>
+            <Row>
+                <Col lg={9} xs={0}>
+                    <div className={"img-container"}>
+                        <figure className="position-relative">
+                            <img src={wallpaper} className="img-fluid"/>
+                            <figcaption className="citation-div">
+                                    <div className="citation text-white">
+                                        ” <br></br>
+                                        &nbsp;&ensp;Shopping
+                                        <span className="secondhand"> secondhand</span>
+                                        <br></br>
+                                        &ensp;&nbsp;isn't a sacrifice. It's <br></br>&nbsp;&ensp;your ticket to a <span
+                                        className="secondhand">good life</span>.
+                                        <br></br>
+                                        &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;„
+                                        <br></br>
+                                        &emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&ensp;<span className="textSpan">- Nicole Lapin</span>
+                                    </div>
+                            </figcaption>
+                        </figure>
+                    </div>
+                </Col>
+                <Col className={"login-container"} lg={3} xs={12}>
+                    <div>
+                        {!props.formOpen &&
+                            <LoginForm handleLoginClick={props.handleLoginClick} handleRegisterClick={props.handleRegisterClick}/>
+                        }
+                        {props.formOpen &&
+                            <div>
+                                <RegisterForm handleRegisterClick={props.handleRegisterClick}/>
+                            </div>
+                        }
+                    </div>
+                </Col>
+            </Row>
+        </div>
+    )
+}
+
+
 
 export default App;
