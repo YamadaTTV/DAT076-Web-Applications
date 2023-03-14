c11b5afc50a91651170a1c4697bc197fb3c306ac
Pipipipiano <marcusphu@gmail.com>
2023-03-14 15:52:53 +0100

    Updated Product_test and Ongoing SellForm_test
    Updated Product_test and Ongoing SellForm_test

--- client/src/components/componentTests/Product.test.tsx
+++ client/src/components/componentTests/Product.test.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import {act, fireEvent, render} from '@testing-library/react';
 import {IProduct, Product} from '../Product';
+import {Pages} from "../../App";
+import {IndexPage} from "../../pages/IndexPage";
+import {ProductPage} from "../../pages/ProductPage";
+import axios, {AxiosStatic} from "axios";
 
 const handleTest = (product: IProduct) => {
     console.log("handleCart iahsdiashdjksas");
@@ -9,16 +13,53 @@ const handleTest2 = () => {
     console.log("handleCart iahsdiashdjksas");
 }
 
+jest.mock('axios');
+const mockedAxios = axios as jest.Mocked<AxiosStatic>
+
+beforeEach( async() => {
+    mockedAxios.get.mockResolvedValue({
+        data: [{"id":1,"username":"1","email":"1@123.com","password":"1"}],
+        status: "200",
+        statusText: "OK"
+
+    });
+
+    mockedAxios.post.mockResolvedValue({
+        data: [{
+            key: 1,
+            productName:"soffa",
+            productDescription:"En fin soffa",
+            productCategory:"Furniture",
+            price:123,
+            sellerId:1
+        }],
+        status: "202",
+        statusText: "Accepted"
+
+    });
+})
+
 test('Test creation of a product.', async() => {
-    const soffa: IProduct = {key: 1, productName:"soffa",productDescription:"En fin soffa av hög kvalitet. Köpt på IKEA. Lite sliten men inte nersutten.",productCategory:"möbel",price:123,sellerId:1}
+    let component;
+    const soffa: IProduct = {key: 1, productName:"soffa",productDescription:"En fin soffa",productCategory:"Furniture",price:123,sellerId:1}
+
+    await act(() => {
+        // fire events that update state
+        component = render(<Product prod={soffa} productHandler={() => {}} handlePage={() => {}} page={Pages.PRODUCT}>{}</Product>)
+    });
+
     const bed: IProduct = {key: 2, productName:"Bed",productDescription:"Perfect bed. Made for sleeping in.",productCategory:"möbel",price:321,sellerId:1}
-    //render(<Product prod={soffa} handleCart={handleTest} handleDeleteClick={handleTest2}>{}</Product>)
-    expect(screen.getByText(/En fin soffa av hög kvalitet./i)).toBeInTheDocument()
-    expect(screen.getByText(/123 KR/i)).toBeInTheDocument()
-    let buttons = await screen.findAllByRole('button')
+
+    // @ts-ignore
+    expect(component.getByText(/En fin soffa./i)).toBeInTheDocument()
+    // @ts-ignore
+    expect(component.getByText(/123 KR/i)).toBeInTheDocument()
+
+
+    // @ts-ignore
+    let buttons = await component.findAllByRole('button')
     expect(buttons).toHaveLength(2)
-    //render(<Product prod={bed} handleCart={handleTest} handleDeleteClick={handleTest2}>{}</Product>)
-    buttons = await screen.findAllByRole('button')
-    expect(buttons).toHaveLength(4)
-    expect(screen.getByText(/Perfect bed. Made for sleeping in./i)).toBeInTheDocument()
 });
+
+
+
